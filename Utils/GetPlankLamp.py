ae194a021ae808db2b8a65cc8000440dd980e0a0
lirik <dimdimych181007@yandex.ru>
2025-07-19 11:59:32 +0300

    Plank func 2 commit

--- Utils/GetPlankLamp.py
+++ Utils/GetPlankLamp.py
@@ -3,7 +3,6 @@ from math import exp, pi
 def plank_func(t, length):
     c1 = 3.7412 * 10 ** -16
     c2 = 1.4388 * 10 ** -2
-
     e0 = c1 / (length ** 5 * (exp(c2 / (length * t)) - 1))
     return e0 * 10 ** -9
 
